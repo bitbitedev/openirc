0d7e3062196fdfb86c9014a244ba53cea566b8bf
thatsnasu <motzkus.hauke@gmail.com>
2022-10-27 05:17:27 +0200

    some linting, client progress

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # OpenIRC - Open source IRC library
+
 [![Build](https://github.com/ThatsNasu/OpenIRC/actions/workflows/gradle.yml/badge.svg?branch=release)](https://github.com/ThatsNasu/OpenIRC/actions/workflows/gradle.yml)
 [![Version](https://img.shields.io/github/v/release/ThatsNasu/OpenIRC?display_name=tag&include_prereleases&label=Release)](https://github.com/ThatsNasu/OpenIRC/releases)
 
@@ -8,17 +9,22 @@ Since the RFC2812 standard superseeded the [RFC1459](https://rfc-editor.org/rfc/
 ---
 
 ## Adding this library as dependency
+
 is pretty straight forward. Just grab the jar file and add it to your projects dependencies, or use one of the following build tools.
 
 ### If you are using gradle
+
 add the following line to your dependencies:
-```
+
+```groovy
 implementation 'dev.thatsnasu:OpenIRC:0.1.0
 ```
 
 ### For Maven
+
 it would be
-```
+
+```xml
 <dependency>
     <groupId>dev.thatsnasu</groupId>
     <artifactId>OpenIRC</artifactId>
@@ -27,4 +33,5 @@ it would be
 ```
 
 ### For everything else
+
 Visit [maven central](https://search.maven.org/artifact/dev.thatsnasu/OpenIRC) and pick the release you like to find the code you need.
--- src/main/java/dev/thatsnasu/openirc/Client.java
+++ src/main/java/dev/thatsnasu/openirc/Client.java
@@ -8,70 +8,152 @@ import java.io.PrintWriter;
 import java.net.Socket;
 
 public abstract class Client implements Runnable {
+	private ConnectionConfiguration connectionConfiguration;
+
 	protected Socket socket;
 	protected BufferedReader reader;
 	protected PrintWriter writer;
 
 	private boolean connected;
-	private boolean running;
 
 
-	public Client() {
-		this.running = true;
-	}
+	public Client() {}
 
 	public void connect(ConnectionConfiguration connectionConfiguration) throws IOException, IRCException {
-		this.socket = new Socket(connectionConfiguration.getHost(), connectionConfiguration.getPort());
+		this.connectionConfiguration = connectionConfiguration;
+		this.socket = new Socket(this.connectionConfiguration.getHost(), this.connectionConfiguration.getPort());
 		this.reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 		this.writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
-		this.sendRawLine("PASS "+connectionConfiguration.getAuthentication());
-		this.sendRawLine("NICK "+connectionConfiguration.getNickname());
-		this.sendRawLine("USER "+connectionConfiguration.getUsername());
+		this.sendRawLine("PASS "+this.connectionConfiguration.getAuthentication());
+		this.sendRawLine("NICK "+this.connectionConfiguration.getNickname());
+		this.sendRawLine("USER "+this.connectionConfiguration.getUsername());
 	}
 
 	public final void sendRawLine(String data) {
+		System.out.println("Sending: \""+data+"\" to server");
 		this.writer.println(data);
 		this.writer.flush();
 	}
 
-	// returns true if the event will be consumed, false if we dont know what the message means
-	private final boolean handleLine(String line) {
-		String[] splits = line.split(" ");
-		// assume splits_0 is prefix, splits_1 is the responsecode and splits_2 the response
-		if(splits[1].equals("004")) {
-			this.connected = true; // got message of the day, so we must be connected;
-			return true;
+	public final void quitServer() {
+		try {
+			this.reader.close();
+			this.writer.close();
+			this.socket.close();
+		} catch(IOException e) {
+			e.printStackTrace();
 		}
-		System.out.println(line);
-		return false;
+		this.onQuitRequest();
+	}
+
+	protected void sendMessage(String target, String message) {
+		this.sendRawLine("PRIVMSG "+target+" "+message);
+	}
+
+	protected void joinChannel(String channel, String password) {
+		String line = (password != null) ? "JOIN "+channel+" "+password : "JOIN "+channel;
+		this.sendRawLine(line);
+		this.onJoinAttempt();
 	}
 
 	@Override
 	public void run() {
 		String line;
-		while(this.running) {
-			try {
-				while((line = this.reader.readLine()) != null) {
-					this.handleLine(line);
-				}
-				// we got here, so we must have been disconnected
-				this.connected = false;
-				this.onDisconnect();
-			} catch (IOException e) {
-				e.printStackTrace();
+		try {
+			while((line = this.reader.readLine()) != null) {
+				this.handleLine(line);
 			}
+		} catch (IOException e) {
+			this.connected = false;
+			this.onDisconnect();
 		}
 	}
 
-	public final void quitServer() {
-		this.running = false;
-		this.onQuitRequest();
+	protected void requestNameList(String... channels) {
+		String listBuilder = "NAMES ";
+		for(String listItem : channels) listBuilder += listItem+",";
+		this.sendRawLine(listBuilder);
+	}
+
+	private final boolean handleLine(String line) {
+		System.out.println(line);
+		String[] splits = line.split(" ");
+
+		if(splits[0].equals("PING")) {
+			this.onServerPing(splits[1]);
+			return true;
+		}
+
+		String sourceNick = null;
+		String sourceLogin = null;
+		String sourceHost = null;
+		String senderMetaData = splits[0].substring(1);
+		String response = splits[1];
+		String target = splits[2];
+		String content = null;
+		if(line.chars().filter(ch -> ch == ':').count() >= 2) {
+			content = line.substring(line.indexOf(":", line.indexOf(":")+1));
+		}
+		
+		int exclamation = senderMetaData.indexOf("!");
+		int at = senderMetaData.indexOf("@");
+		if(exclamation > 0 && at > 0 && exclamation < at) {
+			sourceNick = senderMetaData.substring(1, exclamation);
+			sourceLogin = senderMetaData.substring(exclamation+1, at);
+			sourceHost = senderMetaData.substring(at+1);
+		}
+
+		switch(response) {
+			case "004":
+				this.connected = true; // got server info, so we must be connected;
+				this.onServerInfo(sourceNick, target, content);
+				return true;
+			case "353":
+				String[] list = content.split(" ");
+				for(String nickname : list) this.onJoin(nickname, nickname, sourceHost, target);
+				return true;
+			case "366":
+				this.onEndOfNames();
+				return true;
+			case "JOIN":
+				this.onJoin(sourceNick, sourceLogin, sourceHost, target);
+				return true;
+			case "PART":
+				this.onPart(sourceNick, sourceLogin, sourceHost, target);
+				return true;
+			case "PRIVMSG":
+				this.onMessage(sourceNick, sourceLogin, sourceHost, target, content);
+				return true;
+			default:
+				// something like log unknown stuff, maybe firing an onUnknown() event.
+				// CAP * ACK on CAP REQ (i guess something like capabilities acknowledged)
+				return false;
+		}
 	}
 
 	public final boolean isConnected() {
 		return this.connected;
 	}
 
+	protected void onServerPing(String token) {
+		this.sendRawLine("PONG "+token);
+	}
+
 	protected void onDisconnect() {}
 	protected void onQuitRequest() {}
+	protected void onWelcome() {}
+	protected void onYourHost() {}
+	protected void onCreation() {}
+	protected void onServerInfo(String sourceNick, String target, String content) {}
+	protected void onMessageOfTheDayStart() {}
+	protected void onMessageOfTheDay() {}
+	protected void onMessageOfTheDayEnd() {}
+	protected void onJoinAttempt() {}
+	protected void onJoin(String sourceNick, String sourceLogin, String sourceHost, String target) {}
+	protected void onJoinSuccess() {}
+	protected void onJoinFailed() {}
+	protected void onUserPing() {}
+	protected void onPart(String sourceNick, String sourceLogin, String sourceHost, String target) {}
+	protected void onEndOfNames() {}
+	protected void onMessage(String sourceNick, String sourceLogin, String sourceHost, String target, String content) {}
 }
