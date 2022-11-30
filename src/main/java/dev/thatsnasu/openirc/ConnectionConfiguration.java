bbce72ae83dd885ff6cbef165eaf240de7c57ec3
thatsnasu <motzkus.hauke@gmail.com>
2022-11-30 16:32:46 +0100

    made ConnectionConfiguration values public

--- src/main/java/dev/thatsnasu/openirc/ConnectionConfiguration.java
+++ src/main/java/dev/thatsnasu/openirc/ConnectionConfiguration.java
@@ -1,13 +1,14 @@
 package dev.thatsnasu.openirc;
 
 public class ConnectionConfiguration {
-	private String authentication;
-	private String username;
-	private String nickname;
-	private String realname;
+	public String authentication;
+	public String username;
+	public String nickname;
+	public String realname;
 
-	private String host;
-	private int port;
+	public String host;
+	public int port;
+	public int timeout;
 
 
 	public ConnectionConfiguration() {
@@ -18,55 +19,6 @@ public class ConnectionConfiguration {
 
 		this.host = null;
 		this.port = 6667;
-	}
-
-	public final void setAuthentication(String authentication) {
-		this.authentication = authentication;
-	}
-
-	public final void setUsername(String username) {
-		this.username = username;
-		if(this.nickname.equals("OpenIRC")) this.nickname = this.username;
-	}
-
-	public final void setNickname(String nickname) {
-		this.nickname = nickname;
-		if(this.username.equals("OpenIRC")) this.username = this.nickname;
-	}
-
-	public final void setRealName(String realname) {
-		this.realname = realname;
-	}
-
-	public final void setHost(String host) {
-		this.host = host;
-	}
-
-	public final void setPort(int port) {
-		this.port = port;
-	}
-
-	public final String getUsername() {
-		return this.username;
-	}
-
-	public final String getNickname() {
-		return this.nickname;
-	}
-
-	public final String getRealname() {
-		return this.realname;
-	}
-
-	public final String getAuthentication() {
-		return this.authentication;
-	}
-
-	public final String getHost() {
-		return this.host;
-	}
-
-	public final int getPort() {
-		return this.port;
+		this.timeout = 15000;
 	}
 }
