88f720eee6fc80ddf32cc4aaf83d79c12464b182
Hauke Motzkus <38265230+ThatsNasu@users.noreply.github.com>
2024-01-26 23:48:07 +0100

    Client send messages
     - Closes #10

--- src/main/java/dev/thatsnasu/openirc/IRCClient.java
+++ src/main/java/dev/thatsnasu/openirc/IRCClient.java
@@ -3,9 +3,9 @@ package dev.thatsnasu.openirc;
 import java.net.URL;
 
 import dev.bitbite.networking.Client;
+import dev.thatsnasu.openirc.exceptions.IRCMessagingException;
 
 public class IRCClient extends Client {
-
 	
 	public IRCClient(URL url) {
 		super(url);
@@ -19,5 +19,14 @@ public class IRCClient extends Client {
 	protected void processReceivedData(byte[] data) {
 		
 	}
-
+	
+	public void sendMessage(String target, String message) throws IRCMessagingException {
+		if(target == null) throw new IRCMessagingException("Could not process message, target is null.");
+		if(target.equals("")) throw new IRCMessagingException("Could not process message, target is empty.");
+		if(message == null) throw new IRCMessagingException("Could not process message, null given.");
+		if(message.equals("")) throw new IRCMessagingException("Could not process message, empty string given.");
+		
+		// checks passed
+		this.send(message.getBytes());
+	}
 }
--- src/test/java/dev/thatsnasu/openirc/TestClient.java
+++ src/test/java/dev/thatsnasu/openirc/TestClient.java
@@ -1,8 +1,14 @@
 package dev.thatsnasu.openirc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import dev.thatsnasu.openirc.exceptions.IRCMessagingException;
 
 public class TestClient {
 	private static IRCServer ircServer;
@@ -16,6 +22,20 @@ public class TestClient {
 		TestClient.ircServer.start();
 		
 		TestClient.ircClient = new IRCClient("localhost", 13337);
+		TestClient.ircClient.connect();
+	}
+	
+	@Test
+	@DisplayName("Client sendMessage")
+	public void sendMessage() {
+		assertThrows(IRCMessagingException.class, ()-> TestClient.ircClient.sendMessage(null, null));
+		assertThrows(IRCMessagingException.class, ()-> TestClient.ircClient.sendMessage(null, ""));
+		assertThrows(IRCMessagingException.class, ()-> TestClient.ircClient.sendMessage("", null));
+		assertThrows(IRCMessagingException.class, ()-> TestClient.ircClient.sendMessage("", ""));
+		assertThrows(IRCMessagingException.class, ()-> TestClient.ircClient.sendMessage(null, "Hello World!"));
+
+		assertDoesNotThrow(() -> TestClient.ircClient.sendMessage("thatsnasu", "Hello World!"));
+		assertDoesNotThrow(() -> TestClient.ircClient.sendMessage("#thatsnasu", "Hello World!"));
 	}
 	
 	@AfterAll
