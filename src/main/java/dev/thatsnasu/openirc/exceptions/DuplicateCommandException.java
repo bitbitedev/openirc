2ece3c51da72442542deb04053c982238fa71b53
Hauke Motzkus <38265230+ThatsNasu@users.noreply.github.com>
2024-01-27 18:23:48 +0100

    added automatic command loading

---
+++ src/main/java/dev/thatsnasu/openirc/exceptions/DuplicateCommandException.java
@@ -0,0 +1,9 @@
+package dev.thatsnasu.openirc.exceptions;
+
+public class DuplicateCommandException extends Exception {
+	private static final long serialVersionUID = 1L;
+	
+	public DuplicateCommandException(String message) {
+		super(message);
+	}
+}
