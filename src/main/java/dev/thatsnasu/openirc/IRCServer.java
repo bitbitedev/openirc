24afeefbcc0db85a8e7128a4d575227538d787c4
netcodedev <benmondini@netcode.dev>
2024-02-12 16:33:20 +0100

    Improved regex to cover some edgecases
    - Added edge case that prevents the matching of the message if the command starts with a `:`
    - Changed some groups to be non-capturing
    - Added comment explaining the different parts of the regex

--- src/main/java/dev/thatsnasu/openirc/IRCServer.java
+++ src/main/java/dev/thatsnasu/openirc/IRCServer.java
@@ -45,13 +45,23 @@ public class IRCServer extends Server {
 	}
 
 	protected static Message parseMessage(String message) throws MessagePrefixException, MessageLengthExceededException, MalformedMessageException {
-		Pattern messagePattern = Pattern.compile("^(:(.*?) )?([^: ][^ ]+){1}( (.*))?$");
+		/*
+		 * ^(?::([^ ]*?) ) 	- matches the start of the line, and captures the prefix (if present). 
+		 * 						The prefix is a string that does not contain any spaces and starts with a colon.
+		 * 						The prefix is captured without the colon
+		 * ([^: ][^ ]+){1} 	- matches the command, and captures it. 
+		 * 						The command is a string that does not start with a colon (so that it doesn't get captured as a prefix)
+		 * 						and does not contain any spaces
+		 * (?: (.*))?$ 		- matches the parameters, and captures them. 
+		 * 						The parameters can contain any character
+		 */
+		Pattern messagePattern = Pattern.compile("^(?::([^ ]*?) )?([^: ][^ ]+){1}(?: (.*))?$");
 		Matcher matcher = messagePattern.matcher(message);
 
 		if(matcher.find()){
-			String prefix = matcher.group(2);
-			String command = matcher.group(3);
-			String params = matcher.group(5);
+			String prefix = matcher.group(1);
+			String command = matcher.group(2);
+			String params = matcher.group(3);
 
 			return new Message(prefix, command, params);
 		}
